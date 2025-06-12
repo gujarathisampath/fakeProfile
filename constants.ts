4bf3f70d4b2488e51b67c2906d5537d13ab399bd
Sampath Gujarathi <gujarathi.sampath@gmail.com>
2025-06-12 20:48:44 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -10,4 +10,4 @@ export const SKU_ID_DISCORD = "100101099222222";
 export const SKU_ID = "100101099222224";
 export const GUILD_ID = "1117373291095662623";
 export const INVITE_KEY = "ffmkewQ4R7";
-export const VERSION = "v2.6";
+export const VERSION = "v2.7";
